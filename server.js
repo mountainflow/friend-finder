e2102f2de1f0c3a907aa92db5b989663a8df74d9
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-05-02 13:26:14 -0600

    first

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+// Sets up the Express App=================================
+const app = express();
+// process.env.PORT lets the port be set by Heroku=========
+var PORT = process.env.PORT || 3000;
+
+
+// Just testing stuff======================================
+//=========================================================
+
+app.get('/:id', function (req, res) {
+    res.send(req.params.id);
+});
+
+
+//=========================================================
+//=========================================================
+
+
+// Starts the server to begin listening====================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
