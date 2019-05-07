9004e51df0f0cc983409f0bcc49dc1849e543e4a
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-05-06 23:33:16 -0600

    Everything working except getting the modal to pop up

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,6 +13,54 @@ let friend = [{
         1,
         1
     ]
+},
+{
+    name: 'Bruce Wayne',
+    photo: 'https://www.dccomics.com/sites/default/files/Char_GetToKnow_Batman80_5ca54cb83a27a6.53173051.png',
+    scores: [
+        2,
+        4,
+        2,
+        1,
+        4,
+        4,
+        2,
+        5,
+        1,
+        5
+    ]
+},
+{
+    name: 'Peter Parker',
+    photo: 'https://insomniac.games/wp-content/uploads/2018/09/Spider-Man_PS4_Selfie_Photo_Mode_LEGAL.jpg',
+    scores: [
+        4,
+        5,
+        1,
+        3,
+        4,
+        5,
+        5,
+        3,
+        2,
+        3
+    ]
+},
+{
+    name: 'Thing 1 and Thing 2',
+    photo: 'https://images-na.ssl-images-amazon.com/images/I/51XPN5kWX3L._SX425_.jpg',
+    scores: [
+        5,
+        5,
+        1,
+        5,
+        2,
+        3,
+        5,
+        2,
+        5,
+        5
+    ]
 }];
 
 module.exports = friend;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,9 +5,68 @@ module.exports = function (app) {
         res.json(friends);
     });
 
+
     app.post('/api/friends', function (req, res) {
-        friends.push(req.body);
-        var numConverted = req.body.scores.map(Number);
-        console.log(numConverted);
+
+        // Convert scores to integers before pushing to friends
+        let newUser = req.body;
+        let numConverted = newUser.scores.map(Number);
+        newUser.scores = numConverted;
+
+        let differences = {
+            scores: []
+        };
+
+        let finalArray = [];
+
+        for (let i = 0; i < friends.length; i++) {
+
+            for (let j = 0; j < friends[i].scores.length; j++) {
+                differences.scores.push(Math.abs(newUser.scores[j] - friends[i].scores[j]));
+            }
+            let diff = differences.scores.reduce(function (total, num) {
+                return Number(total) + Number(num);
+            }, 0);
+
+            // console.log('\nDifferences 01 :\n' + JSON.stringify(differences) + '\n');
+
+            differences.scores.length = 0;
+
+            finalArray.push(diff);
+        }
+
+        //Get index of smallest number in finalArray
+        let index = 0;
+        let value = finalArray[0];
+        for (let i = 1; i < finalArray.length; i++) {
+            if (finalArray[i] < value) {
+                value = finalArray[i];
+                index = i;
+            }
+        }
+
+        console.log('\nIndex: ' + index);
+
+        console.log('\nBest Match : ' + JSON.stringify(friends[index]));
+
+        let bestMatch = friends[index];
+
+
+
+        // console.log('\nDifferences :\n' + JSON.stringify(differences) + '\n');
+
+        console.log('finalArray: ' + JSON.stringify(finalArray));
+
+        // Push the newUser to friends with all scores changed to integers
+        friends.push(newUser);
+
+        console.log(friends);
+
+
+
+
+        res.json(bestMatch);
+
+
     });
 };
